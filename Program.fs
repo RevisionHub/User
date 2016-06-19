520eb5e33b7f972536833100d29dbcc8c1571982
Harlan Connor <harlan.connor@gmail.com>
2016-06-19 19:31:57 +0100

    Adding username and TEMPORARY password command

--- Program.fs
+++ Program.fs
@@ -15,12 +15,27 @@ let main argv =
         let payload = (read p).payload
         let pos = Array.findIndex((=) 0uy) payload
         let g,v = Array.splitAt pos payload |> fun (i,j) -> i,j.[1..] // in v.[..v.Length-2]
-        v
-        |> System.Text.ASCIIEncoding.UTF8.GetString
-        |> fun i -> i.Split '&'
-        |> Array.map(fun i -> let i' = i.Split('=') in System.Web.HttpUtility.HtmlDecode(i'.[0]),System.Web.HttpUtility.HtmlDecode(i'.[1]))
-        |> printfn "%A"
-        {payloadsize=g.Length+1;payloadverb=PayloadVerb.Get;payload=g}|>write c.[Daemon.Page]
+        let vars = 
+            v
+            |> System.Text.ASCIIEncoding.UTF8.GetString
+            |> fun i -> i.Split '&'
+            |> Array.map(fun i -> let i' = i.Split('=') in System.Web.HttpUtility.HtmlDecode(i'.[0]),System.Web.HttpUtility.HtmlDecode(i'.[1]))
+        printfn "%A" vars 
+        let vars' = 
+            [|
+                for i,j in vars do 
+                    yield Array.concat
+                        [|
+                            [|0uy|]
+                            System.Text.ASCIIEncoding.UTF8.GetBytes(i)
+                            [|0uy|]
+                            System.Text.ASCIIEncoding.UTF8.GetBytes(j)
+                        |]
+            |]
+            |> Array.concat
+            |> Array.append g
+        printfn "%A" vars'
+        {payloadsize=vars'.Length;payloadverb=PayloadVerb.GotPost;payload=vars'}|>write c.[Daemon.Page]
     printfn "%A" argv
     0 // return an integer exit code
 
