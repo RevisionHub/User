48a89a6f02263941846299f195065618517703de
Harlan Connor <harlan.connor@gmail.com>
2016-06-21 09:09:47 +0100

    Adding hash passwords

--- Program.fs
+++ Program.fs
@@ -1,4 +1,24 @@
 ﻿open CommonLib
+let user_db = "/home/harlan/website_ssl/user.db"
+let users = 
+    try 
+        System.IO.File.ReadAllLines(user_db)|>Array.map(fun (i) -> let j = i.Split(',') in j.[0],j.[1]) |> Map.ofArray
+    with |e -> failwithf "Could not open the user database:\n%A" e
+let conversions = 
+    [
+        "password",
+            (System.Text.ASCIIEncoding.UTF8.GetBytes : string -> byte[])
+            >> System.Security.Cryptography.SHA512Cng.Create().ComputeHash
+            >> System.Convert.ToBase64String
+    ]
+    |> Map.ofList<string,(string->string)>
+let convertfrompost (s:string) =
+    let s = s.Replace('+',' ')
+    System.Text.RegularExpressions.Regex("\%").Matches s
+    |> Seq.cast
+    |> Seq.map (fun (i:System.Text.RegularExpressions.Match) -> i.Index)
+    |> Seq.fold(fun s i -> String.concat "" [s.[..i-1]; int("0x"+s.[i+1..i+2])|>char|>string;s.[i+3..]]) s
+
 [<EntryPoint>]
 let main argv = 
     let c = 
@@ -19,8 +39,8 @@ let main argv =
             v
             |> System.Text.ASCIIEncoding.UTF8.GetString
             |> fun i -> i.Split '&'
-            |> Array.map(fun i -> let i' = i.Split('=') in System.Web.HttpUtility.HtmlDecode(i'.[0]),System.Web.HttpUtility.HtmlDecode(i'.[1]))
-        printfn "%A" vars 
+            |> Array.map (fun i -> let i' = i.Split('=') in convertfrompost(i'.[0]),convertfrompost(i'.[1]))
+            |> Array.map (fun (i,j) -> Map.tryFind i conversions |> function |None -> i,j |Some(k) -> i,k j)
         let vars' = 
             [|
                 for i,j in vars do 
@@ -34,6 +54,8 @@ let main argv =
             |]
             |> Array.concat
             |> Array.append g
+            |> Array.append 
+            <| [|0uy|]
         printfn "%A" vars'
         {payloadsize=vars'.Length;payloadverb=PayloadVerb.GotPost;payload=vars'}|>write c.[Daemon.Page]
     printfn "%A" argv
